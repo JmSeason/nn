85029aaab56042313ce676783593f84174f00782
JmSeason <JmYangSeason@hotmail.com>
2017-05-25 23:35:51 +0800

    layer

--- layer_creater.py
+++ layer_creater.py
@@ -1,19 +1,20 @@
 import tensorflow as tf
 
-def create_mlp_layer(inputs, in_size, out_size, activation_function=None):
+def create_mlp_layer(in_size, out_size, activation_function=None):
     '''
-    :param inputs: 
     :param in_size: 
     :param out_size: 
     :param activation_function: 
     :return: 
     '''
     # create layer and return the Weights, biases, output of this layer
+    xs = tf.placeholder(tf.float32, [None, 1])
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    Wx_plus_b = tf.matmul(xs, Weights) + biases
     if activation_function is None:
         outputs = Wx_plus_b
     else:
         outputs = activation_function(Wx_plus_b)
-    return Weights, biases, outputs
+    return xs, Weights, biases, outputs
+
