21557f6f751207c35b93f7fcec13f587c4091af2
JmSeason <JmYangSeason@hotmail.com>
2017-06-02 19:01:41 +0800

    add gen test

---
+++ gan_test.py
@@ -0,0 +1,25 @@
+import numpy as np
+from keras.layers import Dense, Input
+import matplotlib.pyplot as plt
+
+# Hyper Parameters
+BATCH_SIZE = 64
+LR_G = 0.0001
+LR_D = 0.0001
+N_IDEAS = 5         # number of ideas for generating an art work (Generator)
+ART_COMPONENTS = 15  # it could be total point G can draw in the canvas
+PAINT_POINTS = np.vstack([np.linspace(-1, 1, ART_COMPONENTS) for _ in range(BATCH_SIZE)])
+
+#show painting range
+plt.plot(PAINT_POINTS[0], 2 * np.power(PAINT_POINTS[0], 2) + 1, c='#74BCFF', lw=3, label='upper bound')
+plt.plot(PAINT_POINTS[0], 1 * np.power(PAINT_POINTS[0], 2) + 0, c='#FF9359', lw=3, label='lower bound')
+plt.legend(loc='upper right')
+plt.show()
+
+def target_works():  # real target
+    a = np.random.uniform(1, 2, size=BATCH_SIZE)[:, np.newaxis]
+    paintings = a * np.power(PAINT_POINTS, 2) + (a - 1)
+    return paintings
+
+#build model
+input_layer = Input(shape=())
